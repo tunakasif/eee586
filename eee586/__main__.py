0eec8860c25cf78ccf35d03bcac7209be19f9272
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-05-02 01:44:01 +0300

    Implement CLI with click for package

--- eee586/__main__.py
+++ eee586/__main__.py
@@ -1,4 +1,43 @@
+import click
 import torch
-import torch_geometric
 
-print(torch.cuda.is_available())
+from eee586.pretrain import pretrain_bert_model
+from eee586.word_embedding import embed_sentence
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--dataset-sample-size",
+    default=1000,
+    help="Number of samples to use.",
+    type=click.INT,
+)
+def pretrain(dataset_sample_size):
+    pretrain_bert_model(
+        dataset_sample_size=dataset_sample_size,
+    )
+
+
+@cli.command()
+@click.option(
+    "--sentence",
+    default="Replace me by any text you'd like.",
+    help="Sentence to embed.",
+    type=click.STRING,
+)
+def embed(sentence):
+    output = embed_sentence(sentence)
+    print(output[0].shape)
+
+
+@cli.command()
+def check_torch_cuda():
+    print(torch.cuda.is_available())
+
+
+cli()
