84c84e855fdf0eddaec3d2c1fcc1281eac108d47
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-05-03 02:45:39 +0300

    Add pickle functions

--- eee586/__init__.py
+++ eee586/__init__.py
@@ -5,4 +5,5 @@ __version__ = "0.1.0"
 SRC_DIR = Path(__file__).parent.absolute()
 WORK_DIR = SRC_DIR.parent
 BERT_MODEL_DIR = Path.joinpath(WORK_DIR, "bert_models")
+PKL_DIR = Path.joinpath(WORK_DIR, "pkl")
 BERT_DEFAULT_MODEL_NAME = "distilbert-base-uncased"
