7a8b73325b73e34faa8137cabe88f4475f63238a
ardaaras99 <ardaaras99@gmail.com>
2022-05-05 03:53:41 +0300

    2 Configs for Training
    First approach obtains graph both from test and train.
    Second one creates graphs individually.

--- gnn_trial.py
+++ gnn_trial.py
@@ -1,178 +1,258 @@
 # %%
-import numpy as np
-import torch
 from scipy.sparse import csr_matrix
-import itertools
 from torch_geometric.data import Data
+from pathlib import Path
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, GATConv
+from scipy.sparse import random
+from torch import tensor
 
 
+#%matplotlib inline
+# import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from eee586 import PKL_DIR, WORK_DIR
 from eee586.word_embedding import get_token_encodings
+from eee586.utils.adjacency import generate_adj_matrix
+from eee586.utils.generic import pickle_dump, pickle_load
 
-train_encodings = get_token_encodings("train")
-test_encodings = get_token_encodings("train")
 # %%
-def get_adjacency_matrix():
-    pass
+def train_func(data, model, optimizer, criterion):
+    model.train()
+    optimizer.zero_grad()
+    out = model.forward()
+    loss = criterion(out[data.train_idx], data.y[data.train_idx])
+    loss.backward()
+    optimizer.step()
+    return loss
+
+
+def test_model(data, model, type):
+    model.eval()
+    out = model()
+    pred = out.argmax(dim=1)
+    if type == "test":
+        correct = pred[data.test_idx] == data.y[data.test_idx]
+        acc = int(correct.sum()) / int(data.test_idx.sum())
+    else:
+        correct = pred[data.train_idx] == data.y[data.train_idx]
+        acc = int(correct.sum()) / int(data.train_idx.sum())
+    return pred, acc * 100
+
+
+def get_edge_values(A):
+    smat = csr_matrix(A)
+    c, n_nodes = smat.tocoo(), A.shape[0]
+    row, col = tensor(c.row).reshape(-1, 1), tensor(c.col).reshape(-1, 1)
+    data = c.data
+    edge_index = torch.concat((row, col), dim=1).T.long().contiguous()
+    edge_attr = tensor(data).reshape(-1)
+    return edge_index, edge_attr, n_nodes
 
 
 def get_graph_data(
-    train_encodings: dict,
-    test_encodings: dict,
-    num_of_train_samples: int = None,
-    num_of_test_samples: int = None,
-):
-    """
-    Return the Data class of the pytorch geometric
-    """
-    train_documents, test_documents = train_encodings.get(
-        "input_ids"
-    ), test_encodings.get("input_ids")
-    train_labels, test_labels = train_encodings.get("labels"), test_encodings.get(
-        "labels"
+    train_encods: dict,
+    test_encods: dict,
+    n_train: int = None,
+    n_test: int = None,
+    window_size=10,
+    stride=1,
+) -> Data:
+
+    train_docs, test_docs = train_encods.get("input_ids"), test_encods.get("input_ids")
+    train_labels, test_labels = train_encods.get("labels"), test_encods.get("labels")
+
+    if n_train and n_test is None:
+        n_train, n_test = len(train_docs), len(test_docs)
+
+    documents = train_docs[:n_train] + test_docs[:n_test]
+    labels = train_labels[:n_train] + test_labels[:n_test]
+
+    A = generate_adj_matrix(
+        documents,
+        dataset_name=f"SetFit/20_newsgroups_ntrain{n_train}_ntest{n_test}",
+        window_size=window_size,
+        stride=stride,
     )
+    print(A)
+    edge_index, edge_attr, n_nodes = get_edge_values(A)
 
-    if num_of_train_samples and num_of_test_samples is None:
-        num_of_train_samples = len(train_documents)
-        num_of_test_samples = len(documents)
-
-    documents = train_documents + test_documents
-    labels = train_labels, test_labels
-
-    vocab_list = [set(document) for document in documents]
-    vocab = list(set().union(*vocab_list))
-
-    A = get_adjacency_matrix(documents)
-    smat = csr_matrix(A)
-    values, column_indices, row_dptrs = smat.data, smat.indices, smat.indptr
-
-    t = []
-    for i in range(0, len(row_dptrs) - 1):
-        repetition = row_dptrs[i + 1] - row_dptrs[i]
-        for j in range(repetition):
-            t.append(i)
-
-    t = torch.tensor(t).reshape(-1, 1)
-    column_indices = torch.tensor(column_indices).reshape(-1, 1)
-
-    edge_index = torch.concat((t, column_indices), dim=1).T
-    edge_attr = torch.tensor(values).reshape(-1, 1)
-
-    num_of_nodes = A.shape[0]
-    x = torch.eye(num_of_nodes)
-    y = torch.tensor(labels)
+    x, y = torch.eye(n_nodes), tensor(labels + (n_nodes - len(labels)) * [0])
 
     data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
-    data.train_idx = torch.arange(len(train_labels))
-    data.test_idx = torch.arange(
-        len(train_labels), len(train_labels) + len(test_labels)
+
+    data.train_idx = tensor(n_train * [True] + (n_nodes - n_train) * [False])
+    data.test_idx = tensor(
+        n_train * [False] + n_test * [True] + (n_nodes - (n_train + n_test)) * [False]
     )
     return data
 
 
-def find_pmi_score(
-    w1_id: int,
-    w2_id: int,
-    documents: list[list[int]],
-    window_lenght: int = 10,
-    stride: int = 1,
-):
-    corpus = list(itertools.chain(*documents))
-    p_i, p_j, p_ij = 0, 0, 0
-    total_num_of_windows = (len(corpus) - window_lenght + 1) / stride
+def get_graph_data_train(
+    train_encods: dict,
+    n_train: int = None,
+    ratio=0.8,
+    window_size=10,
+    stride=1,
+) -> Data:
 
-    if w1_id == w2_id:
-        pmi_score = 1
+    """
+    n_train: used to select subset of train dataset to fit the memory
+    ratio: used to divide train set to train and validation
+    """
+    train_docs, train_labels = train_encods.get("input_ids"), train_encods.get("labels")
+    if n_train is None:
+        n_train = len(train_docs)
+    train_docs, train_labels = train_docs[:n_train], train_labels[:n_train]
+
+    idx = int(len(train_docs) * (ratio))
+
+    documents, labels = train_docs, train_labels
+
+    train_docs, val_docs = train_docs[:idx], train_docs[idx:]
+    train_labels, val_labels = train_labels[:idx], train_labels[idx:]
+    n_val, n_train = len(val_docs), len(train_docs)
+
+    A = generate_adj_matrix(
+        documents,
+        dataset_name=f"SetFit/20_newsgroups_ntrain{n_train}_tv_ratio{ratio}",
+        window_size=window_size,
+        stride=stride,
+    )
+    edge_index, edge_attr, n_nodes = get_edge_values(A)
+    x, y = torch.eye(n_nodes), tensor(labels + (n_nodes - len(labels)) * [0])
+
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+
+    data.train_idx = tensor(n_train * [True] + (n_nodes - n_train) * [False])
+    data.test_idx = tensor(
+        n_train * [False] + n_val * [True] + (n_nodes - (n_train + n_val)) * [False]
+    )
+    return data
+
+
+def get_graph_data_test(
+    test_encods: dict,
+    n_test: int = None,
+    window_size=10,
+    stride=1,
+) -> Data:
+    test_docs, test_labels = test_encods.get("input_ids"), test_encods.get("labels")
+    if n_test is None:
+        n_test = len(test_docs)
+    documents, labels = test_docs[:n_test], test_labels[:n_test]
+
+    A = generate_adj_matrix(
+        documents,
+        dataset_name=f"SetFit/20_newsgroups_ntest{n_test}",
+        window_size=window_size,
+        stride=stride,
+    )
+
+    edge_index, edge_attr, n_nodes = get_edge_values(A)
+
+    x, y = torch.eye(n_nodes), tensor(labels + (n_nodes - len(labels)) * [0])
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+    data.test_idx = tensor(n_test * [True] + (n_nodes - n_test) * [False])
+    data.train_idx = tensor(n_nodes * [False])
+
+    return data
+
+
+# %%
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels, data: Data):
+        super().__init__()
+        self.data = data
+        # self.edge_weight = torch.nn.Parameter(self.data.edge_attr)
+        self.edge_weight = self.data.edge_attr
+        self.conv1 = GCNConv(data.num_node_features, hidden_channels, cached=True)
+        self.conv2 = GCNConv(hidden_channels, 20, cached=True)
+
+    def forward(self):
+        x, edge_index = (self.data.x, self.data.edge_index)
+        x = x.double()
+        x = F.relu(self.conv1(x, edge_index, self.edge_weight))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, edge_index, self.edge_weight)
+        x = F.dropout(x, p=0.5, training=self.training)
+        return x
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, nhid, data: Data, dropout=True):
+        super(GAT, self).__init__()
+        self.data = data
+        self.conv1 = GATConv(data.num_node_features, nhid, heads=1, dropout=dropout)
+        self.conv2 = GATConv(nhid * 1, 20, heads=1, concat=False, dropout=dropout)
+        self.dropout = dropout
+
+    def forward(self):
+        x, edge_index, edge_attr = (
+            self.data.x,
+            self.data.edge_index,
+            self.data.edge_attr,
+        )
+        x = x.double()
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
+
+
+#%%
+def train_strategy(train_encods, test_encods, together=True):
+    """
+    If together is True, we will construct single graph for test and train and mask test during training
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.cuda.empty_cache()
+
+    if together == True:
+        data = get_graph_data(
+            train_encods,
+            test_encods,
+            n_train=100,
+            n_test=40,
+            window_size=20,
+            stride=1,
+        ).to(device)
+        data_train = data
+
     else:
-        for i in range(0, len(corpus) - window_lenght, stride):
-            if w1_id in corpus[i : i + window_lenght]:
-                p_i += 1
-            if w2_id in corpus[i : i + window_lenght]:
-                p_j += 1
-            if (
-                w1_id in corpus[i : i + window_lenght]
-                and w2_id in corpus[i : i + window_lenght]
-            ):
-                p_ij += 1
-        pmi_score = np.log(p_ij / (p_i * p_j * total_num_of_windows))
-    return pmi_score
+        data_train = get_graph_data_train(
+            train_encods, n_train=100, ratio=0.8, window_size=10, stride=1
+        ).to(device)
+        data_test = get_graph_data_test(
+            test_encods, n_test=40, window_size=10, stride=1
+        ).to(device)
+
+    model = GCN(data=data_train, hidden_channels=200).double().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    criterion = torch.nn.CrossEntropyLoss()
+    for epoch in range(1, 1000):
+        loss = train_func(data_train, model, optimizer, criterion)
+        if together == True:
+            _, train_acc = test_model(data_train, model, type="train")
+            _, test_acc = test_model(data_train, model, type="test")
+            print(f"Train Accuracy: {train_acc:.4f}, Test Accuracy: {test_acc:.4f}")
+            if epoch % 10 == 1:
+                print(f"Epoch: {epoch:03d}, Loss: {loss:.4f}")
+
+        else:
+            _, train_acc = test_model(data_train, model, type="train")
+            _, test_acc = test_model(data_test, model, type="test")
+
+            print(f"Train Accuracy: {train_acc:.4f}, Test Accuracy: {test_acc:.4f}")
+            if epoch % 10 == 1:
+                print(f"Epoch: {epoch:03d}, Loss: {loss:.4f}")
+    return model
 
 
-def find_tf_idf_score(
-    documents: list[list[int]],
-    document_index: int,
-    word_id: int,
-):
-    """
-    t — term (word)
-    d — document (set of words)
-    N — count of corpus
-    corpus — the total document set
-    """
-    corpus = np.array(list(itertools.chain(*documents)))
+# %%
+train_encods = get_token_encodings("train")
+test_encods = get_token_encodings("test")
 
-    d = np.array(documents[document_index])
-    tf = np.count_nonzero(d == word_id) / len(d)
-    df = np.count_nonzero(corpus == word_id) / len(d)
-    idf = np.log(len(documents) / (df + 1))
-    tf_idf_score = tf * idf
-    return tf_idf_score
-
-
-# # %%
-
-# # %%
-# from torch_geometric.datasets import Planetoid
-
-# dataset = Planetoid(root="/tmp/Cora", name="Cora")
-# # %%
-# import torch
-# import torch.nn.functional as F
-# from torch_geometric.nn import GCNConv
-
-
-# class GCN(torch.nn.Module):
-#     def __init__(self, edge_weight):
-#         super().__init__()
-#         self.conv1 = GCNConv(dataset.num_node_features, 16)
-#         self.conv2 = GCNConv(16, dataset.num_classes)
-#         self.edge_weight = edge_weight
-
-#     def forward(self, data):
-#         x, edge_index = data.x, data.edge_index
-
-#         x = self.conv1(x, edge_index, self.edge_weight)
-#         x = F.relu(x)
-#         x = F.dropout(x, training=self.training)
-#         x = self.conv2(x, edge_index, self.edge_weight)
-
-#         return F.log_softmax(x, dim=1)
-
-
-# # %%
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# document = None  # get from tuna
-# edge_weight, edge_index = get_csr_matrix(document)
-# model = GCN(edge_weight).to(device)
-# data = dataset[0].to(device)
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
-
-# model.train()
-# for epoch in range(200):
-#     optimizer.zero_grad()
-#     out = model(data)
-#     loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-#     loss.backward()
-#     optimizer.step()
-# # %%
-# model.eval()
-# pred = model(data).argmax(dim=1)
-# correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
-# acc = int(correct) / int(data.test_mask.sum())
-# print(f"Accuracy: {acc:.4f}")
-# # %%
-
-# # %%
-
-# # %%
-
-# # %%
+model = train_strategy(train_encods, test_encods, together=False)
