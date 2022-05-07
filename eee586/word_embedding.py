bb735f42348348f158b746321cad2bdbf3efe830
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-05-07 04:29:51 +0300

    Implement BERT based document embedding

--- eee586/word_embedding.py
+++ eee586/word_embedding.py
@@ -1,5 +1,4 @@
-from pickle import PicklingError
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
 from pathlib import Path
 from datasets import load_dataset
 from tqdm import tqdm
@@ -8,9 +7,128 @@ import nltk
 from nltk.corpus import stopwords
 from itertools import chain
 import numpy as np
+import torch
+import torch.nn.functional as F
 
-from eee586 import BERT_DEFAULT_MODEL_NAME, PKL_DIR
-from eee586.utils.generic import picklize
+from eee586 import BERT_DEFAULT_MODEL_NAME, PKL_DIR, BERT_LAST_HIDDEN_OUT_SIZE
+from eee586.utils.generic import picklize, batch_iterable
+
+
+def _pad_batch_to_max(batch: List[torch.Tensor]) -> torch.Tensor:
+    max_length = max([doc.numel() for doc in batch])
+    batch = [
+        F.pad(doc, pad=(0, max_length - doc.numel()), mode="constant", value=0)
+        for doc in batch
+    ]
+    return torch.concat(batch, dim=0)
+
+
+def _truncate_tensor_last_dim(tensor: torch.Tensor, max_length: int) -> torch.Tensor:
+    curr_last_dim = tensor.shape[-1]
+    return tensor if curr_last_dim <= max_length else tensor[..., :max_length]
+
+
+def _get_doc_embeddings(
+    sub_dataset_name="train",  # train/test
+    *,
+    remove_stop: bool = True,
+    freq_limit: int = None,
+    enforce_recompute=False,
+    model_name=BERT_DEFAULT_MODEL_NAME,
+    dataset_name="SetFit/20_newsgroups",
+    batch_size=1,
+    max_embed_length=2048,
+) -> np.ndarray:
+    if not sub_dataset_name in ["train", "test"]:
+        raise ValueError("sub_dataset must be either 'train' or 'test'")
+
+    sub_token_encodings = get_token_encodings(
+        sub_dataset_name=sub_dataset_name,
+        remove_stopword=remove_stop,
+        freq_limit=freq_limit,
+        enforce_recompute=enforce_recompute,
+        model_name=model_name,
+        dataset_name=dataset_name,
+    )
+
+    documents = sub_token_encodings["input_ids"]
+    max_embed_length = min(max_embed_length, max([len(doc) for doc in documents]))
+    all_vocab = [set(doc) for doc in documents]
+    all_vocab = np.array(list(set.union(*all_vocab)))
+    all_vocab = np.sort(all_vocab)
+
+    documents = [
+        _truncate_tensor_last_dim(torch.tensor([doc]), max_embed_length)
+        for doc in documents
+    ]
+    all_vocab = torch.tensor([all_vocab])
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = BertModel.from_pretrained(
+        model_name,
+        max_position_embeddings=max_embed_length,
+    )
+    model.to(device)
+    model.eval()
+
+    batches = batch_iterable(documents, batch_size=batch_size)
+    embeddings = np.zeros((len(documents), BERT_LAST_HIDDEN_OUT_SIZE))
+    with torch.no_grad():
+        total_count = len(documents) // batch_size
+        for i, batch in enumerate(
+            tqdm(
+                batches,
+                total=total_count,
+                desc="Embedding",
+                unit="batch",
+            )
+        ):
+            batch = _pad_batch_to_max(batch)
+            embedding = model(batch.to(device)).last_hidden_state
+            embedding = torch.mean(embedding, dim=1)
+            row_range = slice(i * batch_size, (i + 1) * batch_size)
+            embeddings[row_range, :] = embedding.cpu().numpy()
+    return embeddings
+
+
+def get_doc_embeddings(
+    sub_dataset_name="train",  # train/test
+    *,
+    remove_stop: bool = True,
+    freq_limit: int = None,
+    enforce_recompute=False,
+    model_name=BERT_DEFAULT_MODEL_NAME,
+    dataset_name="SetFit/20_newsgroups",
+    batch_size=1,
+    max_embed_length=2048,
+) -> np.ndarray:
+    sub_pkl_dir = Path.joinpath(
+        PKL_DIR,
+        dataset_name.replace("/", "_"),
+        model_name,
+        f"maxlen{max_embed_length}_batchsize{batch_size}",
+    )
+    if remove_stop:
+        sub_pkl_dir = sub_pkl_dir.joinpath("wo_stop")
+    if freq_limit is not None:
+        sub_pkl_dir = sub_pkl_dir.joinpath(f"freq_limit_{freq_limit}")
+    Path.mkdir(sub_pkl_dir, parents=True, exist_ok=True)
+    sub_pkl_path = sub_pkl_dir.joinpath(f"{sub_dataset_name}_doc_embeddings.pkl")
+
+    embeddings = picklize(
+        _get_doc_embeddings,
+        sub_pkl_path,
+        sub_dataset_name=sub_dataset_name,
+        remove_stop=remove_stop,
+        freq_limit=freq_limit,
+        enforce_recompute=enforce_recompute,
+        model_name=model_name,
+        dataset_name=dataset_name,
+        batch_size=batch_size,
+        max_embed_length=max_embed_length,
+    )
+
+    return embeddings
 
 
 def _prune_words(
